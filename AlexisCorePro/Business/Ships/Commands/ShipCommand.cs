8a7996bd2eb69cfe2353fb3be5fdb948d1188ff2
Aleksa Stancetic <aleksaas89@gmail.com>
2018-10-11 20:10:44 +0200

    - Validation experimenting

--- AlexisCorePro/Business/Ships/Commands/ShipCommand.cs
+++ AlexisCorePro/Business/Ships/Commands/ShipCommand.cs
@@ -4,6 +4,7 @@ using FluentValidation;
 using Localization.Resources;
 using Microsoft.Extensions.Localization;
 using System;
+using System.Linq;
 
 namespace AlexisCorePro.Business.Ships.Commands
 {
@@ -22,31 +23,44 @@ namespace AlexisCorePro.Business.Ships.Commands
 
     public class ShipCommandValidator : AbstractValidator<ShipCommand>
     {
-        public ShipCommandValidator(IStringLocalizer<SharedResource> stringLocalizer)
+        private DatabaseContext ctx;
+
+        public ShipCommandValidator(IStringLocalizer<SharedResource> stringLocalizer, DatabaseContext ctx)
         {
+            this.ctx = ctx;
+
             RuleFor(cmd => cmd.Name).NotEmpty().MaximumLength(Default.TextFieldLength);
             RuleFor(cmd => cmd.Date).GreaterThan(DateTime.Today);
             RuleFor(cmd => cmd.Imd).NotEmpty();
             RuleFor(cmd => cmd.Mmsi).NotEmpty();
             RuleFor(cmd => cmd.CustomerId).NotEmpty();
             RuleFor(cmd => cmd)
-            .Must(HaveUniqueName)
+            .Must(IsNameUnique)
                 .WithMessage(stringLocalizer["NameUnique"])
-            .Must(MustNotExceedMaxCustomerShipNum)
-                .WithMessage(stringLocalizer["MuxNumberShips"]);
+            .Must(IsMaxNumberReachedForCustomer)
+                .WithMessage(stringLocalizer["MuxNumberShips"])
+            .Must(IsCustomerNotBlacklisted)
+                .WithMessage(stringLocalizer["CustomerBlacklisted"]);
+
+            // RuleFor(cmd => cmd.Name).Must((cmd, name) => name != cmd.Name);
 
             // If first one is not true, then chained one will not be executed, like in the first and last example here because of
             // ValidatorOptions.CascadeMode = CascadeMode.StopOnFirstFailure; in Startup.cs
         }
 
-        private bool HaveUniqueName(ShipCommand cmd)
+        private bool IsNameUnique(ShipCommand cmd)
         {
-            return false;
+            return !ctx.Ships.Any(e => e.Name == cmd.Name);
         }
 
-        private bool MustNotExceedMaxCustomerShipNum(ShipCommand cmd)
+        private bool IsMaxNumberReachedForCustomer(ShipCommand cmd)
         {
-            return true;
+            return ctx.Ships.Where(e => e.CustomerId == cmd.CustomerId).Count() < 3;
+        }
+
+        private bool IsCustomerNotBlacklisted(ShipCommand cmd)
+        {
+            return !ctx.Customers.Where(e => e.Id == cmd.CustomerId).Select(e => e.Blacklisted).First();
         }
     }
 }
--- AlexisCorePro/Domain/Model/Customer.cs
+++ AlexisCorePro/Domain/Model/Customer.cs
@@ -13,6 +13,8 @@ namespace AlexisCorePro.Domain.Model
 
         public ICollection<Ship> Ships { get; set; }
 
+        public bool Blacklisted { get; set; }
+
         [NotMapped]
         [Computed]
         public virtual int ShipNumber => Ships.Count;
